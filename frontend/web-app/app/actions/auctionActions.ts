f3a83959a0756514f272267a5282c7bbf9786b9b
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-09-24 19:42:23 -0600

    Testing API authentication

---
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -0,0 +1,25 @@
+'use server';
+import {PagedResult, Auction} from "@/types";
+
+export async function getDate(query: string): Promise<PagedResult<Auction>> {
+	const res = await 
+	fetch(`http://localhost:6001/search${query}`);
+
+	if(!res.ok) throw new Error('Failed to fetch data');
+
+	return res.json();
+}
+
+export async function updateActionTest(): Promise<{status: number, message: string}> {
+	const data = {
+		mileage: Math.floor(Math.random() * 10000) + 1
+	}
+
+	const res = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
+		method: 'PUT',
+		headers: {},
+		body: JSON.stringify(data)
+	});
+	if (!res.ok) return({status: res.status, message: res.statusText});
+	return {status: res.status, message: res.statusText}
+}
