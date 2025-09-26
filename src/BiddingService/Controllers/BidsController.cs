a16a3049727b45dfcf3c4a1ef324e1941adaa43c
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-09-26 17:38:36 -0600

    Adding a producer for the BidPlaced

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
@@ -13,9 +15,10 @@ namespace BiddingService.Controllers
 {
 	[ApiController]
 	[Route("api/[controller]")]
-	public class BidsController(IMapper mapper) : ControllerBase
+	public class BidsController(IMapper mapper, IPublishEndpoint publishEndpoint) : ControllerBase
 	{
 		private readonly IMapper _mapper = mapper;
+		private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
 
 		[Authorize]
 		[HttpPost]
@@ -64,6 +67,9 @@ namespace BiddingService.Controllers
 			}
 
 			await DB.SaveAsync(bid);
+
+			await _publishEndpoint.Publish(_mapper.Map<BidPlaced>(bid));
+
 			return Ok(_mapper.Map<BidDto>(bid));
 		}
 
